273129ff34769515d5a5312789fed873bd6d8d08
LeeTZ <litiezheng513@gmail.com>
2019-10-24 11:37:49 -0400

    use pd_merge_asof join to test equality of results

--- python/pyspark/sql/tests/test_asofjoin.py
+++ python/pyspark/sql/tests/test_asofjoin.py
@@ -17,35 +17,42 @@
 
 from pyspark.testing.sqlutils import ReusedSQLTestCase
 from pyspark.sql import Row
+import pandas as pd
+from datetime import datetime
 
 class AsofJoinTests(ReusedSQLTestCase):
 
-    def _get_dataframes(self):
-        import pandas as pd
-        from datetime import datetime
+    def data(self):
         pdf1 = pd.DataFrame({"time": [datetime(2001,1,1), datetime(2001,1,1), datetime(2002,1,1)],
                             "id": [1, 2, 1],
                             "v": [1.0, 1.1, 1.2]}, columns=["time", "id", "v"])
-        df1 = self.spark.createDataFrame(pdf1)
         pdf2 = pd.DataFrame({"time": [datetime(2001,1,1), datetime(2001,1,1)],
                             "id": [1, 2],
                             "v2": [4, 5]}, columns=["time", "id", "v2"])
-        df2 = self.spark.createDataFrame(pdf2)
-        return df1, df2
+        return pdf1, pdf2
 
     def test_invalid_join(self):
-        df1, df2 = self._get_dataframes()
+        pdf1, pdf2 = self.data()
+        df1 = self.spark.createDataFrame(pdf1)
+        df2 = self.spark.createDataFrame(pdf2)
         self.assertRaises(TypeError, lambda: df1.asofJoin(df2, df1["time"], df2["id"]).count())
 
     def test_join_with_on_by(self):
-        df1, df2 = self._get_dataframes()
-        actual = df1.asofJoin(df2, df1["time"], df2["time"], df1["id"], df2["id"]).count()
-        self.assertEqual(3, actual)
+        pdf1, pdf2 = self.data()
+        df1 = self.spark.createDataFrame(pdf1)
+        df2 = self.spark.createDataFrame(pdf2)
+        actual = df1.asofJoin(df2, df1["time"], df2["time"], df1["id"],
+            df2["id"]).toPandas().sort_values(by='time').reset_index(drop=True)
+        self.assertTrue(actual.equals(pd.merge_asof(pdf1, pdf2, on="time", by="id")))
 
     def test_join_with_all_params(self):
-        df1, df2 = self._get_dataframes()
-        actual = df1.asofJoin(df2, df1["time"], df2["time"], df1["id"], df2["id"], "1d", True).count()
-        self.assertEqual(3, actual)
+        pdf1, pdf2 = self.data()
+        df1 = self.spark.createDataFrame(pdf1)
+        df2 = self.spark.createDataFrame(pdf2)
+        actual = df1.asofJoin(df2, df1["time"], df2["time"], df1["id"], df2["id"],
+            "1d", True).toPandas().sort_values(by='time').reset_index(drop=True)
+        self.assertTrue(actual.equals(pd.merge_asof(pdf1, pdf2, on="time", by="id",
+            tolerance=pd.Timedelta('1d'), allow_exact_matches=True)))
 
 if __name__ == "__main__":
     import unittest
